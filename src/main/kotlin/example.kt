b3323f2e594347a06e191fc9151d0333bc3b40bc
Nyannazz <kantemir.imam@gmail.com>
2019-09-23 16:55:07 +0200

    metaballs 1 ball working

--- src/main/kotlin/example.kt
+++ src/main/kotlin/example.kt
@@ -5,57 +5,83 @@ import org.openrndr.draw.*
 import org.openrndr.math.Vector2
 import kotlin.random.Random
 
-class Ball(x: Double,y:Double, glowRadius: Double, movement : Vector2){
-    var x : Double=0.0
-    var y : Double=0.0
-    var glowRadius : Double=10.0
-    var movement : Vector2
+fun randomReverse(input: Double, from: Double, until: Double): Double {
+    return if (input >= 0.0) Random.nextDouble(from = from, until = until) * -1 else Random.nextDouble(
+        from = from,
+        until = until
+    )
+}
+
+class Ball(pos: Vector2, glowRadius: Double, movement: Vector2) {
+    var pos: Vector2 = Vector2(0.0, 0.0)
+    var glowRadius: Double = 10.0
+    var movement: Vector2
+    var colorShift: Vector2
+    var lastCollision: String
+
     init {
-        this.x=x
-        this.y=y
-        this.glowRadius=glowRadius
-        this.movement=movement
+        this.pos = pos
+        this.glowRadius = glowRadius
+        this.movement = movement
+        this.colorShift = Vector2(Random.nextDouble(until = 1.0), Random.nextDouble(until = 1.0))
+        this.lastCollision = ""
     }
-    fun updatePos(moveX: Double, moveY: Double){
-        this.x+=moveX
-        this.y+=moveY
+
+    fun updatePos() {
+        this.pos += this.movement
+    }
+
+    fun collisionEvent(top: Double, right: Double, bottom: Double, left: Double) {
+        var rememberCollision: String = this.lastCollision
+        if (this.pos.y < top) {
+            if (lastCollision != "top") {
+                this.lastCollision = "top"
+                this.movement = Vector2(this.movement.x, randomReverse(this.movement.y, 4.0, 14.0))
+
+            }
+
+        }
+        if (this.pos.x > right) {
+            if (this.lastCollision != "right") {
+                this.lastCollision = "right"
+                this.movement = Vector2(randomReverse(this.movement.x, 4.0, 14.0), this.movement.y)
+
+            }
+        }
+        if (this.pos.y > bottom) {
+            if (lastCollision != "bottom") {
+                this.lastCollision = "bottom"
+                this.movement = Vector2(this.movement.x, randomReverse(this.movement.y, 4.0, 14.0))
+
+            }
+        }
+        if (this.pos.x < left) {
+            if (this.lastCollision != "left") {
+                this.lastCollision = "left"
+                this.movement = Vector2(randomReverse(this.movement.x, 4.0, 14.0), this.movement.y)
+
+            }
+
+        }
+        if (rememberCollision != this.lastCollision) {
+            this.colorShift = Vector2(Random.nextDouble(until = 1.0), Random.nextDouble(until = 1.0))
+            this.glowRadius = Random.nextDouble(from = 4.0, until = 20.0)
+        }
     }
 
 }
 
-fun checkCol(x: Double, y: Double, moveX: Double, moveY: Double, w: Int, h: Int, cX: Double, cY: Double, size: Double) : Array<Double>{
-    var moveXOut : Double=moveX;
-    var moveYOut : Double=moveY;
-    var colX : Double=cX
-    var colY : Double=cY
-    var mySize : Double=size
-    if(x>w.toDouble() || x<0.0){
-        moveXOut*=-1.0;
-        colX=Random.nextDouble(until=1.0)
-        mySize=Random.nextDouble(from=2.0 ,until=40.0)
-    }
-    if(y>h.toDouble() || y<0.0){
-        moveYOut*=-1.0;
-        colY=Random.nextDouble(until=1.0)
-        mySize=Random.nextDouble(from=2.0 ,until=40.0)
-
-    }
-
-    //currentCol=Vector2(Random.nextDouble(until=1.0),Random.nextDouble(until=1.0))
-    return arrayOf(moveXOut, moveYOut, colX,colY, mySize)
-
-}
 
 fun main() = application {
     //var newBall=Ball(160.0, 280.0, 20.0)
 
-    var ballX=460.0;
-    var ballY=640.0;
-    var ballMoveX=/*Random.nextDouble(until=10.0)-10.0*/ 10.0;
-    var ballMoveY=/*Random.nextDouble(until=10.0)-10.0*/ 8.0;
-    var currentCol=Vector2(Random.nextDouble(until=1.0),Random.nextDouble(until=1.0))
-    var mySize : Double= 20.0
-
+    var ballX = 460.0;
+    var ballY = 640.0;
+    var ballMoveX =/*Random.nextDouble(until=10.0)-10.0*/ 10.0;
+    var ballMoveY =/*Random.nextDouble(until=10.0)-10.0*/ 8.0;
+    var currentCol = Vector2(Random.nextDouble(until = 1.0), Random.nextDouble(until = 1.0))
+    var mySize: Double = 20.0
+    var ballArr: ArrayList<Ball>
     configure {
         width = 1080
         height = 1080
@@ -66,44 +92,56 @@ fun main() = application {
     }
 
     program {
-        extend{
-            //for(x in 0..10){
-            //    drawer.circle(x=Random.nextDouble(until = width.toDouble()), y=Random.nextDouble(until = height.toDouble()), radius= Random.nextDouble(until = 160.0));
-            //}
-            val newMove=checkCol(ballX,ballY,ballMoveX,ballMoveY,width,height,currentCol.x,currentCol.y,mySize)
+        /*CREATE SOME BALLS*/
+        var ballList: MutableList<Ball> = ArrayList()
+        for (i in 0..3) {
+            ballList.add(
+                Ball(
+                    Vector2(
+                        Random.nextDouble(until = width.toDouble()),
+                        Random.nextDouble(until = height.toDouble())
+                    ),
+                    Random.nextDouble(until = 10.0),
+                    Vector2(Random.nextDouble(until = 10.0), Random.nextDouble(until = 10.0))
+                )
+            )
+        }
 
-            ballMoveX=newMove[0]
-            ballMoveY=newMove[1]
-            currentCol=Vector2(newMove[2],newMove[3])
-            mySize=newMove[4]
-            ballX+=ballMoveX;
-            ballY+=ballMoveY;
-
-
-
-
-            drawer.shadeStyle= shadeStyle {  fragmentTransform="""
-                |float multiply=p_size;
+        extend {
+            drawer.shadeStyle = shadeStyle {
+                fragmentTransform = """
+                |float glowRadius=p_size;
                 |vec2 normBallPos=p_ballPos/p_resolution;
                 |vec2 normScreenPos=c_screenPosition/p_resolution;
                 |float ballDistance=distance(normBallPos, normScreenPos);
                 |
-                |float multiplyDistance=ballDistance*multiply;
+                |float multiplyDistance=ballDistance*100.0;
                 |
-                |x_fill.rgba=vec4(vec3((1.0/multiplyDistance)*vec3(p_colVec,1.0)),1.0);
+                |float sumColor=glowRadius/multiplyDistance;
+                |
+                |//x_fill.rgba=vec4(vec3((1.0/multiplyDistance)*vec3(p_colVec,1.0)),1.0);
+                |x_fill.rgba=vec4(vec3((sumColor)*vec3(p_colVec,1.0)),1.0);
             """.trimMargin()
-                parameter("resolution", Vector2(width.toDouble(),height.toDouble()))
-                parameter("ballPos", Vector2(ballX,ballY))
-                parameter("colVec",currentCol)
-                parameter("size",mySize)
-                //parameter("ballPosTwo", Vector2(ballX,ballY))
-                //parameter("colVec",currentCol)
-                //parameter("sizeTwo",mySize)
+                parameter("resolution", Vector2(width.toDouble(), height.toDouble()))
+                parameter("colVec", ballList[0].colorShift)
+
+                parameter("ballPos", ballList[0].pos)
+                parameter("size", ballList[0].glowRadius)
+                parameter("ballPosTwo", ballList[1].pos)
+                parameter("sizeTwo", ballList[1].glowRadius)
+                parameter("ballPosThree", ballList[2].pos)
+                parameter("sizeThree", ballList[2].glowRadius)
+
             }
-            drawer.fill= ColorRGBa.PINK
-            drawer.rectangle(0.0,0.0,width.toDouble(),height.toDouble())
+            drawer.fill = ColorRGBa.PINK
+            drawer.rectangle(0.0, 0.0, width.toDouble(), height.toDouble())
+            for (ball in ballList) {
+                ball.collisionEvent(0.0, width.toDouble(), height.toDouble(), 0.0)
+                ball.updatePos()
+            }
         }
     }
 
 
 }
+
